8a9bfa3172b616e6419ed662f1813fd9b8d45799
Daniel Tan <d.tan@aims.gov.au>
2024-09-11 15:55:26 +1000

    use integer for region zones not Floats

--- src/classification/region_classification.jl
+++ src/classification/region_classification.jl
@@ -43,9 +43,9 @@ end
 inner_loc = [contained_in(inner_shelf.geom, cent) for cent in centroids]
 outer_loc = [contained_in(outer_shelf.geom, cent) for cent in centroids]
 
-features[!,         :shelf_position] .= 2.0
-features[outer_loc, :shelf_position] .= 3.0
-features[inner_loc, :shelf_position] .= 1.0
+features[!,         :shelf_position] .= 2
+features[outer_loc, :shelf_position] .= 3
+features[inner_loc, :shelf_position] .= 1
 
 shape_file = "C:/users/dtan/data/ltmp_shape/gbr_3Zone 2.shp"
 
@@ -60,10 +60,10 @@ central_mask = BitVector([AG.contains(region_shps.convex_hull[2], cent) for cent
 south_mask = BitVector([AG.contains(region_shps.convex_hull[3], cent) || AG.gety(cent, 0) < -21.0 for cent in centroids])
 not_contained = (!).(north_mask .|| central_mask .|| south_mask)
 
-features[!,            :ltmp_region] .= 0.0
-features[north_mask,   :ltmp_region] .= 1.0
-features[central_mask, :ltmp_region] .= 2.0
-features[south_mask,   :ltmp_region] .= 3.0
+features[!,            :ltmp_region] .= 0
+features[north_mask,   :ltmp_region] .= 1
+features[central_mask, :ltmp_region] .= 2
+features[south_mask,   :ltmp_region] .= 3
 
 @info "Writing updated location data to $(OUTPUT_CSV)"
 CSV.write(OUTPUT_CSV, features)
