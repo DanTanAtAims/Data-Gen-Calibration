b0ee2bd5f713e15229dd43b60c9c8e767a46e485
Daniel Tan <d.tan@aims.gov.au>
2024-09-12 11:09:43 +1000

    remove utils.jl add helpers to main script
    add docstring to utils helpers

--- src/reefmod_loc_obs/overview.jl
+++ src/reefmod_loc_obs/overview.jl
@@ -1,4 +1,3 @@
-include("utils.jl")
 include("common.jl")
 using CSV,
     Dates,
@@ -7,7 +6,120 @@ using CSV,
     YAXArrays,
     NetCDF
 
+# Helpers
+"""
+    lb_simple(encoding::AbstractString)::Union{Missing, Float64}
 
+Convert median cover encodings to Floats. Each encoding corresponds to a range. This
+function returns the lower bound.
+"""
+function lb_simple(encoding::AbstractString)::Union{Missing, Float64}
+    tmp = encoding
+    if length(encoding) == 1 && encoding[1] != '0'
+        tmp *= "L"
+    end
+    encodings::Dict{String, Union{Missing, Float64}} = Dict(
+        "" => missing,
+        "0"  => 0.0,
+        "1L" => 1.0,
+        "1U" => 5.0,
+        "2L" => 10.0,
+        "2U" => 20.0,
+        "3L" => 30.0,
+        "3U" => 40.0,
+        "4L" => 50.0,
+        "4U" => 62.5,
+        "5L" => 75.0,
+        "5U" => 87.5
+    )
+    return missing
+end
+"""
+    ub_simple(encoding::AbstractString)::Union{Missing, Float64}
+
+Convert median cover encodings to Floats. Each encoding corresponds to a range. This
+function returns the upper bound.
+"""
+function ub_simple(encoding::AbstractString)::Union{Missing, Float64}
+    tmp = encoding
+    if length(encoding) == 1 && encoding[1] != '0'
+        tmp *= "U"
+    end
+    encodings::Dict{String, Union{Missing, Float64}} = Dict(
+        "" => missing,
+        "0"  => 0.0,
+        "1L" => 5.0,
+        "1U" => 10.0,
+        "2L" => 20.0,
+        "2U" => 30.0,
+        "3L" => 40.0,
+        "3U" => 50.0,
+        "4L" => 62.5,
+        "4U" => 75.0,
+        "5L" => 87.5,
+        "5U" => 100.0
+    )
+    return encodings[tmp]
+end
+"""
+    lower_bound(encoding::Union{Missing, AbstractString})::Union{Missing, Float64}
+
+Convert the encoding for median covert to Floats. Allows for combined encodings.
+"""
+function lower_bound(encoding::Union{Missing, AbstractString})::Union{Missing, Float64}
+    ismissing(encoding) && return missing
+    tmp = encoding
+    if contains(tmp, '/')
+        tmp = split(tmp, '/')[1]
+    end
+    return lb_simple(tmp)
+end
+
+"""
+    upper_bound(encoding::Union{Missing, AbstractString})::Union{Missing, Float64}
+
+Convert the encoding for median covert to Floats. Allows for combined encodings.
+"""
+function upper_bound(encoding::Union{Missing, AbstractString})::Union{Missing, Float64}
+    ismissing(encoding) && return missing
+    tmp = encoding
+    if contains(encoding, '/')
+        tmp = split(encoding, '/')[2]
+    end
+
+    return ub_simple(tmp)
+end
+
+"""
+    create_var(df, var_name; T=Float64)
+
+Create key value pairs for use in dataset construct in YAXArrays.
+"""
+function create_var(df, var_name; T=Float64)
+    tsteps = sort(unique(df.REPORT_YEAR))
+    location_ids = unique(df.REEF_ID)
+
+    axlist = (
+        Dim{:timesteps}(tsteps),
+        Dim{:locs}(String.(location_ids))
+    )
+
+    preallocation = Matrix{Union{Missing, T}}(
+        missing,
+        length(tsteps),
+        length(location_ids)
+    )
+
+    for (idx, loc_id) in enumerate(location_ids)
+        sub_df   = df[df.REEF_ID .== loc_id, :]
+        idx_mask = [findfirst(x -> x == l, tsteps) for l in sub_df.REPORT_YEAR]
+        preallocation[idx_mask, idx] .= sub_df[:, var_name]
+    end
+
+    return var_name => YAXArray(axlist, preallocation)
+end
+
+# Data transformations
 ltmp_site_data = CSV.read(LTMP_PHOTO_FN, DataFrame)
 manta_tow_data = CSV.read(LTMP_MANTA_FN, DataFrame)
 
@@ -46,34 +158,10 @@ vars = [
     :MEAN_DEAD_CORAL,
 ]
 
-function create_var(df, var_name; T=Float64)
-    tsteps = sort(unique(df.REPORT_YEAR))
-    location_ids = unique(df.REEF_ID)
-
-    axlist = (
-        Dim{:timesteps}(tsteps),
-        Dim{:locs}(String.(location_ids))
-    )
-
-    preallocation = Matrix{Union{Missing, T}}(
-        missing,
-        length(tsteps),
-        length(location_ids)
-    )
-
-    for (idx, loc_id) in enumerate(location_ids)
-        sub_df   = df[df.REEF_ID .== loc_id, :]
-        idx_mask = [findfirst(x -> x == l, tsteps) for l in sub_df.REPORT_YEAR]
-        preallocation[idx_mask, idx] .= sub_df[:, var_name]
-    end
-
-    return var_name => YAXArray(axlist, preallocation)
-end
-
 
 var_creater = x -> create_var(manta_tow_data, x)
-
 manta_tow = Dataset(; var_creater.(vars)...)
+
 @info "Writing manta tow observations for reefmod location to $(OUT_MANTA_NETCDF)"
 savedataset(manta_tow; path=OUT_MANTA_NETCDF, driver=:netcdf, overwrite=true)
 
